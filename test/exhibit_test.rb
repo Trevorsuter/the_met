230a94a2433f2f73e789e9f9aac56f594b6b8bea
Trevor Suter <Trevorsuter@icloud.com>
2021-01-07 09:11:45 -0700

    exhibit and exhibit test

---
+++ test/exhibit_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require 'pry'
+
+class ExhibitTest < Minitest::Test
+
+  def test_it_exists_with_attributes
+    ex = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    assert_instance_of Exhibit, ex
+    assert_equal "Gems and Minerals", ex.name
+    assert_equal 0, ex.cost
+  end
+  
+end
